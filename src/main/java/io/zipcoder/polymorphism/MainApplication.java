25fbce355dd8fcc7f5623973715d6758008ae298
Gowri <gowri@ZipcodersMBP64.local>
2023-12-06 23:03:41 -0500

    completed

--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -1,5 +1,7 @@
 package io.zipcoder.polymorphism;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainApplication {
@@ -9,13 +11,37 @@ public class MainApplication {
         Scanner scanner = new Scanner(System.in);
         int noOfPets = scanner.nextInt();
         scanner.nextLine();
-        System.out.println("No of pets are: "+ noOfPets);
-        for(int i = 0; i<noOfPets;i++){
+        System.out.println("No of pets are: " + noOfPets);
+        List<Pet> list = new ArrayList<Pet>();
+        for (int i = 0; i < noOfPets; i++) {
             System.out.println("What kind of pets each one is?");
             String pet = scanner.nextLine();
-            System.out.println("Pet's Name: ");
-            String  petName = scanner.nextLine();
+            if (pet.equals("cat")) {
+                Pet pet1 = new Cat();
+                list.add(pet1);
+                System.out.println("Pet's Name: ");
+                String petName = scanner.nextLine();
+                pet1.setPetName(petName);
+            } else if (pet.equals("dog")) {
+                Pet pet1 = new Dog();
+                list.add(pet1);
+                System.out.println("Pet's Name: ");
+                String petName = scanner.nextLine();
+                pet1.setPetName(petName);
+            } else {
+                Pet pet1 = new Fish();
+                list.add(pet1);
+                System.out.println("Pet's Name: ");
+                String petName = scanner.nextLine();
+                pet1.setPetName(petName);
+            }
         }
+        for (int i =0; i < list.size(); i++){
+            System.out.println(list.get(i).getPetName());
+            System.out.println(list.get(i).speak());
+
+        }
+
 
     }
 
