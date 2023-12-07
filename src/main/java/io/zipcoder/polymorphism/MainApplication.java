b17a055ab6727c3db3cc2da7f7f28e442ff6651a
Gowri <gowri@ZipcodersMBP64.local>
2023-12-06 21:07:14 -0500

    part1 completed

--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -1,4 +1,22 @@
 package io.zipcoder.polymorphism;
 
+import java.util.Scanner;
+
 public class MainApplication {
+
+    public static void main(String[] args) {
+        System.out.println("How many pets are there?");
+        Scanner scanner = new Scanner(System.in);
+        int noOfPets = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("No of pets are: "+ noOfPets);
+        for(int i = 0; i<noOfPets;i++){
+            System.out.println("What kind of pets each one is?");
+            String pet = scanner.nextLine();
+            System.out.println("Pet's Name: ");
+            String  petName = scanner.nextLine();
+        }
+
+    }
+
 }
